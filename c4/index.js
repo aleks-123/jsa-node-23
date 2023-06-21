22edf307b300c387cf05008f20327c669ef8ffe4
aleks-123 <ace.ki@hotmail.com>
2023-06-21 20:29:44 +0200

    c4 promise

--- c4/index.js
+++ c4/index.js
@@ -1,3 +1,10 @@
+//! DA PROCHITATE OD DOKUMENTACIJA
+//? Da istrazite uste nekolku methodi na fs modulot
+//? Kako da izbrishite fajl, kako da preimenuvatefajls
+//? i uste eden po zelba
+//? isto taka da kreirate fajl
+//? i da prochitate fajl
+
 const fs = require("fs");
 
 //! Vchituvanje na podatok
@@ -52,11 +59,92 @@ fs.writeFile("./asyhronWrite.txt", asyhronPodatok, "utf-8", (err) => {
   console.log("uspesno");
 });
 
+fs.writeFile("./asyhronWrite.txt", asyhronPodatok, "utf-8", (err) => {
+  if (err) console.log("Greska");
+  console.log("uspesno");
+});
+
 //! Promise pattern
 
-//! DA PROCHITATE OD DOKUMENTACIJA
-//? Da istrazite uste nekolku methodi na fs modulot
-//? Kako da izbrishite fajl, kako da preimenuvatefajls
-//? i uste eden po zelba
-//? isto taka da kreirate fajl
-//? i da prochitate fajl
+//* Чекор 1
+// const fileWrite = () => {};
+
+//* Чекор 2
+//? Funkcijata kje ni vrati nov Promise objekt
+// const fileWrite = () => {
+//   return new Promise();
+// };
+
+//* Чекор 3
+// const fileWrite = () => {
+//   //? Kje kreirame executor funkcija vo promisot
+//   return new Promise(() => {});
+// };
+
+//* Чекор 4
+// const fileWrite = () => {
+//   //? Kje vnesime vba parametri koi se callback funkcii
+// moze da gi zabelizte definirani kako resolve, reject
+//   return new Promise((success, fail) => {});
+// };
+
+//* Чекор 5
+// const fileWrite = () => {
+//   //? kje ja zapishime fs.writeFile funkcijata vnatre vo egzekucionata funkcija
+//   return new Promise((success, fail) => {
+//     fs.writeFile("data.txt", "Helloo!!!", (err) => {
+//       if (err) return console.log(err);
+//       console.log("Zapisot e napraven");
+//     });
+//   });
+// };
+
+//* Чекор 6
+// const fileWrite = () => {
+//   //? vo ovoj slucaj ako imame err da aktivira fail funkcijata
+//   //? ako imame uspesno zapishan fajl da se aktivira funkcijata success
+//   return new Promise((success, fail) => {
+//     fs.writeFile("data.txt", "Helloo!!!", (err) => {
+//       if (err) return fail(err);
+//       return success();
+//     });
+//   });
+// };
+
+//* Чекор 7
+//? i kje go modificirame fileWrite za da go zema filename,
+//? i data kako parametri
+//? i zemenite parametri gi prenesuvame na fs.writeFile
+//? vo ovoj slucaj ako imame err da aktivira fail funkcijata
+//? ako imame uspesno zapishan fajl da se aktivira funkcijata success
+
+const fileWrite = (filename, data) => {
+  return new Promise((success, fail) => {
+    fs.writeFile(filename, data, "utf-8", (err) => {
+      if (err) return fail(err);
+      return success();
+    });
+  });
+};
+
+const fileRead = (filename) => {
+  return new Promise((success, fail) => {
+    fs.readFile(filename, "utf-8", (err, data) => {
+      if (err) return fail(err);
+      return success(data);
+    });
+  });
+};
+
+const main = async () => {
+  try {
+    await fileWrite("data1.txt", "Asynhron zapis od promis");
+    await fileWrite("data2.txt", "Asynhron zapis od promis 2");
+    const rezultat = await fileRead("super.txt");
+    console.log(rezultat);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+main();
